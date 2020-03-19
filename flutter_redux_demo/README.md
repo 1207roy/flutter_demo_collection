7196397ac536dd25c0e462eb10c10fb0e9b7f167
Roy <ankitroy1207@gmail.com>
2020-03-19 13:12:51 +0530

    Initial commit for flutter_redux_demo project

---
+++ flutter_redux_demo/README.md
@@ -0,0 +1,3 @@
+# flutter_redux_demo
+
+This project is demo of Redux state management using flutter_redux package (https://pub.dev/packages/flutter_redux)
---
+++ flutter_redux_demo/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(FlutterReduxApp());
+}
+
+class FlutterReduxApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
