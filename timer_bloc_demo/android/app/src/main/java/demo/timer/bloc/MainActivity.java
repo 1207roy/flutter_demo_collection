9009de2fa49d52d85a4d468fe44cdd3f4c12dfb6
Roy <ankitroy1207@gmail.com>
2020-03-20 18:02:47 +0530

    Initial commit for timer_bloc_demo project

---
+++ timer_bloc_demo/android/app/src/main/java/demo/timer/bloc/MainActivity.java
@@ -0,0 +1,6 @@
+package demo.timer.bloc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ timer_bloc_demo/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timer Bloc Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'Timer Bloc Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
